f06441ac7dcdf7bd060f798d35852cedbbeef827
Raghav Kaul <raghavkaul@nevada.unr.edu>
2019-10-03 15:35:26 -0700

    Correlation: Create images that correspond to the reports we want

--- Spatial_Filtering_Correlation.py
+++ Spatial_Filtering_Correlation.py
@@ -1,18 +1,21 @@
+from Lib import spatially_filtered, Kernel
+from PGM import PGMImage
+
+
 def do_correlation(pgm_filename, mask_filename):
-    class CorrelationKernel(Kernel):
-        @classmethod
-        def from_pgm(cls, pgm_filename) -> Kernel:
-            p = PGMImage(pgm_filename)
-            mask = []
-            for row in p.pixels:
-                mask.append([b for b in row])
-            return cls(mask)
+    pmask = PGMImage(mask_filename)
 
-        k = CorrelationKernel.from_pgm(mask_filename)
+    def correlation_kernel(p: PGMImage):
+        mask = []
+        for row in p.pixels:
+            mask.append([b for b in row])
+        return Kernel(mask)
 
-        p2: PGMImage = spatially_filtered(pgm_filename, k)
+    k = correlation_kernel(pmask)
 
-        p2.save(f"correlated-to-{mask_filename}-{pgm_filename}")
+    p2: PGMImage = spatially_filtered(pgm_filename, k)
+
+    p2.save(f"correlated-to-{pmask.name}-{p2.name}")
 
 
 if __name__ == "__main__":
