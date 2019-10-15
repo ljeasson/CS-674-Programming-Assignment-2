38530866faad7d97d4db0e6cd1c951be954dcffb
Raghav Kaul <raghavkaul@nevada.unr.edu>
2019-10-15 16:11:44 -0700

    Bug fixes
    * Use a separate buffer to hold modified pixel values
    * Use pixel value 0 for padding, instead of pixel value at index 0

--- Lib.py
+++ Lib.py
@@ -1,6 +1,6 @@
 import ctypes
 
-from ctypes import c_char, c_float, pointer as c_pointer_to, POINTER as PointerT
+from ctypes import c_char, c_double, pointer as c_pointer_to, POINTER as PointerT
 from dataclasses import dataclass
 from typing import List
 
@@ -46,11 +46,13 @@ def spatially_filtered_ffi(pgm_filename: str, k: Kernel) -> PGMImage:
     C_PGMRowT = c_char * p.cols
     C_PGMImageT = PointerT(C_PGMRowT) * p.rows
 
-    C_KernelRowT = c_float * k.cols
+    C_KernelRowT = c_double * k.cols
     C_KernelT = PointerT(C_KernelRowT) * k.rows
 
     c_p = c_2d_arr_from_pyobj(p.pixels, C_PGMImageT, C_PGMRowT)
-    c_p_2 = c_2d_arr_empty(C_PGMImageT, C_PGMRowT, p.rows, p.cols)
+    c_p_2 = c_2d_arr_empty(
+        PointerT(c_double * p.cols) * p.rows, c_double * p.cols, p.rows, p.cols
+    )
     c_k = c_2d_arr_from_pyobj(k.mask, C_KernelT, C_KernelRowT)
 
     spatialfilter = ctypes.cdll.LoadLibrary("./spatialfilter.so")
@@ -59,7 +61,7 @@ def spatially_filtered_ffi(pgm_filename: str, k: Kernel) -> PGMImage:
 
     p2 = PGMImage(pgm_filename)
     for i in range(p.rows):
-        p2.pixels[i] = b"".join([c_p_2[i][0][j] for j in range(p2.cols)])
+        p2.pixels[i] = [c_p_2[i][0][j] for j in range(p2.cols)]
 
     return p2
 
@@ -94,18 +96,25 @@ def spatially_filtered(pgm_filename: str, k: Kernel) -> PGMImage:
                 for t in range(k.cols):
                     x = _x + s
                     y = _y + t
-                    # Pad edges of the image with zeros
 
+                    # Pad edges of the image with zeros
                     if x < 0 or x >= p1.rows or y < 0 or y >= p1.cols:
-                        p_pxl = 0
+                        orig_image_x_y = 0
                     else:
-                        p_pxl = p1.pixels[x][y]
-                    # print(f"i = {i} j = {j} s = {s} t = {t} x = {x} y = {y}")
-                    pxl += p_pxl * k.mask[s][t]
-            # print(pxl)
+                        orig_image_x_y = p1.pixels[x][y]
+                    weighted_pxl = orig_image_x_y * k.mask[s][t]
+
+                    if False:
+                        weighted_pxl = coerce(weighted_pxl)
+
+                    pxl += weighted_pxl
+
+            if False:
+                pxl = coerce(pxl)
             new_row.append(pxl)
 
-        p2.pixels[i] = b"".join([bytes([coerce(b)]) for b in new_row])
+        p2.pixels[i] = [int(b) for b in new_row]
+
 
     return p2
 
--- spatialfilter.c
+++ spatialfilter.c
@@ -1,13 +1,13 @@
 #include <inttypes.h>
 
 void apply_spatial_filter(unsigned char **orig_image,
-                          unsigned char **final_image, uint16_t width,
-                          uint16_t height, float **kernel, uint16_t k_rows,
+                          double **final_image, uint16_t width,
+                          uint16_t height, double **kernel, uint16_t k_rows,
                           uint16_t k_cols);
 
 void apply_spatial_filter(unsigned char **orig_image,
-                          unsigned char **final_image, uint16_t width,
-                          uint16_t height, float **kernel, uint16_t k_rows,
+                          double **final_image, uint16_t width,
+                          uint16_t height, double **kernel, uint16_t k_rows,
                           uint16_t k_cols) {
   uint8_t half_k_rows = (uint8_t)(k_rows / 2);
   uint8_t half_k_cols = (uint8_t)(k_cols / 2);
@@ -20,12 +20,14 @@ void apply_spatial_filter(unsigned char **orig_image,
           uint16_t x = i - half_k_rows + s;
           uint16_t y = j - half_k_cols + t;
 
-          if (x < 0 || x >= height)
-            x = 0;
-          if (y < 0 || y >= width)
-            y = 0;
+          unsigned char orig_image_x_y;
 
-          float weighted_pxl = orig_image[x][y] * kernel[s][t];
+          if (x < 0 || x >= height || y < 0 || y >= width)
+            orig_image_x_y = 0;
+          else
+            orig_image_x_y = orig_image[x][y];
+
+          double weighted_pxl = orig_image_x_y * kernel[s][t];
           if (weighted_pxl > 255)
             weighted_pxl = 255;
           if (weighted_pxl < 0)
