bad72be138dec6a892910f9b2917a2e2178fe7de
Lee Easson <ljeasson@go.olemiss.edu>
2019-10-16 00:18:01 -0700

    Add distort_image() to Median_Filtering.py

--- Median_Filtering.py
+++ Median_Filtering.py
@@ -1,14 +1,41 @@
+from Lib import spatially_filtered, Kernel
 from PGM import PGMImage
-import numpy as np
-import math
-import cv2 as cv
+import random
 
-def median_filtering(image, mask_size):
-    return
+def distort_image(p, percentage):
+    # Percentage of the total pixels
+    pixel_percentage = p.n_pixels * percentage
 
-def median_averaging(image, mask_size):
-    return
+    # Set random percentage of pixels
+    # to white or black
+    i = 0
+    while i < pixel_percentage:
+        white_or_black = random.randint(0,1)
 
+        if white_or_black == 1:
+            p.pixels[random.randint(0,p.rows)][random.randint(0,p.cols)] = 255
+        else:
+            p.pixels[random.randint(0,p.rows)][random.randint(0,p.cols)] = 0
+        i += 1
+    
+    return p
+
+def median_filtering(pgm_filename, mask_size):
+    p = PGMImage(pgm_filename)
+    
+    p_1 = distort_image(p, .50)
+    p_2 = distort_image(p, .70)
+
+
+    
+def median_averaging(pgm_filename, mask_size):
+    p = PGMImage(pgm_filename)
+    
+    p_1 = distort_image(p, .50)
+    p_2 = distort_image(p, .70)
+
+    
+    
 if __name__ == "__main__":
     for img in ("images/lenna.pgm", "images/boat.pgm"):
         for i in (7, 15):
--- Spatial_Filtering_Smoothing.py
+++ Spatial_Filtering_Smoothing.py
@@ -1,8 +1,7 @@
 from Lib import spatially_filtered, Kernel
 from PGM import PGMImage
 
-import numpy as np
-
+'''
 average_matrix_7 = Kernel(mask = [ [1,1,1,1,1,1,1],
                                    [1,1,1,1,1,1,1],
                                    [1,1,1,1,1,1,1],
@@ -26,6 +25,7 @@ average_matrix_15 = Kernel(mask = [ [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
                                     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
                                     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
                                     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] ] * (1/7))
+'''
 
 gaussian_matrix_7 = Kernel(mask=[ [1,1,2,2,2,1,1], 
                                   [1,2,2,4,2,2,1], 
@@ -35,6 +35,47 @@ gaussian_matrix_7 = Kernel(mask=[ [1,1,2,2,2,1,1],
                                   [1,2,2,4,2,2,1], 
                                   [1,1,2,2,2,1,1] ])
 
+gaussian_matrix_15 = Kernel(mask = [ [2,2,3,4,5,5,6,6,6,5,5,4,3,2,2],
+                                     [2,3,4,5,7,7,8,8,8,7,7,5,4,3,2],
+                                     [3,4,6,7,9,10,10,11,10,10,9,7,6,4,3],
+                                     [4,5,7,9,10,12,13,13,13,12,10,9,7,5,4],
+                                     [5,7,9,11,13,14,15,16,15,14,13,11,9,7,5],
+                                     [5,7,10,12,14,16,17,18,17,16,14,12,10,7,5],
+                                     [6,8,10,13,15,17,19,19,19,17,15,13,10,8,6],
+                                     [6,8,11,13,16,18,19,20,19,18,16,13,11,8,6],
+                                     [6,8,10,13,15,17,19,19,19,17,15,13,10,8,6],
+                                     [5,7,10,12,14,16,17,18,17,16,14,12,10,7,5],
+                                     [5,7,9,11,13,14,15,16,15,14,13,11,9,7,5],
+                                     [4,5,7,9,10,12,13,13,13,12,10,9,7,5,4],
+                                     [3,4,6,7,9,10,10,11,10,10,9,7,6,4,3],
+                                     [2,3,4,5,7,7,8,8,8,7,7,5,4,3,2],
+                                     [2,2,3,4,5,5,6,6,6,5,5,4,3,2,2] ])
+
+def smooth_image_averaging(pgm_filename):
+    p = PGMImage(pgm_filename)
+    
+    #p_average_7 = spatially_filtered(pgm_filename, average_matrix_7)
+    #p_average_7.save(f"smoothed_averaging-7-{p.name}")
+
+    #p_average_15 = spatially_filtered(pgm_filename, average_matrix_15)
+    #p_average_15.save(f"smoothed_averaging-15-{p.name}")
+    
+def smooth_image_gaussian(pgm_filename):
+    p = PGMImage(pgm_filename)
+
+    p_gaussian = spatially_filtered(pgm_filename, gaussian_matrix_7)
+    p_gaussian.save(f"smoothed_gaussian-7-{p.name}") 
+
+    p_gaussian = spatially_filtered(pgm_filename, gaussian_matrix_15)
+    p_gaussian.save(f"smoothed_gaussian-15-{p.name}")    
+
+
+if __name__ == "__main__":
+    for img in ("images/lenna.pgm", "images/sf.pgm"):
+        smooth_image_averaging(img)
+        smooth_image_gaussian(img)
+
+'''
 def make_averaging_filter(size):
     matrix = np.ones((size,size), dtype=float)
     avg_matrix = (1/size) * matrix
@@ -46,31 +87,7 @@ def make_gaussian_filter(sigma):
     mu = 0.0
     gaussian_matrix = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
     return gaussian_matrix
-    
 
-def smooth_image_averaging(pgm_filename, sigma):
-    p = PGMImage(pgm_filename)
-    
-    p_average_7 = spatially_filtered(pgm_filename, average_matrix_7)
-    p_average_7.save(f"smoothed_averaging-sigma{sigma}-{p.name}")
-
-    p_average_15 = spatially_filtered(pgm_filename, average_matrix_15)
-    p_average_15.save(f"smoothed_averaging-sigma{sigma}-{p.name}")
-    
-def smooth_image_gaussian(pgm_filename, sigma):
-    p = PGMImage(pgm_filename)
-    p_gaussian = spatially_filtered(pgm_filename, gaussian_matrix_7)
-    p_gaussian.save(f"smoothed_gaussian-sigma{sigma}-{p.name}")    
-
-
-if __name__ == "__main__":
-    for img in ("images/lenna.pgm", "images/sf.pgm"):
-        for i in (7, 15):
-            smooth_image_averaging(img, i)
-        for j in (7, 15):
-            smooth_image_gaussian(img, j)
-
-'''
 img = cv2.imread(image)
 blur = cv2.blur(img,(5,5))
 
